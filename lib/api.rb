b26af4bf0f6bc9e67181361ef894095ecdffa344
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-27 18:39:03 -0400

    first time working

--- lib/api.rb
+++ lib/api.rb
@@ -1,27 +1,91 @@
 require 'net/http'
 require 'uri'
 require "current_games/version"
+require 'json'
+
 
 
 module Api
-  class Body
+
+$game_id = []
+
+  class Top_Thirty
+
+# @@game_id = []
+#
+#     def self.game_id
+#       @@game_id
+#     end
 
     def api_call
 
-uri = URI.parse("https://api.twitch.tv/helix/games/top?first=30")
+      uri = URI.parse("https://api.twitch.tv/helix/games/top?first=30")
+      request = Net::HTTP::Get.new(uri)
+      request["Client-Id"] = "212gsg4xr17yp12of3kmw7sha2f121"
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+        end
+
+        a = response.body
+        b = JSON.parse(a)
+        c = b["data"]
+        games = []
+        ids = []
+
+        d = c.each_with_index {|hash,i| games << "#{i}. " + hash["name"]}
+        e = c.each_with_index {|hash,i| ids << hash["id"]}
+
+        puts games
+        puts "Welcome! Here is a list of today's most popular games. Enter a number 1 - 30 to get a URL where you can watch that game live."
+
+        f = gets.chomp
+        g = f.to_i
+        h = ids[g]
+
+      $game_id << h
+
+      end
+    end
+
+class Top_Player
+
+#def game_id
+#  Top_Thiry.game_id
+#end
+
+  def api_call
+
+x = $game_id[0]
+uri = URI.parse("https://api.twitch.tv/helix/streams?game_id=#{x}&first=1")
 request = Net::HTTP::Get.new(uri)
 request["Client-Id"] = "212gsg4xr17yp12of3kmw7sha2f121"
 
 req_options = {
-  use_ssl: uri.scheme == "https",
+use_ssl: uri.scheme == "https",
 }
 
 response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
 http.request(request)
 end
-puts response.body
+a = response.body
+
+b = JSON.parse(a)
+
+ c = b["data"]
+ d = c[0]
+ e = d["user_name"]
+puts "Copy and paste this URL into your browser to watch a live game of : https://www.twitch.tv/#{e}"
 
 
 end
+
 end
+
+
+
 end
