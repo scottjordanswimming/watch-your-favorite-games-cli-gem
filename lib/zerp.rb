e4c368b288de3277e1e1da2f7186eb2b91a56373
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-26 22:07:11 -0400

    boilerplate

---
+++ lib/zerp.rb
@@ -0,0 +1,6 @@
+require "zerp/version"
+
+module Zerp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/zerp_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Zerp do
+  it "has a version number" do
+    expect(Zerp::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
