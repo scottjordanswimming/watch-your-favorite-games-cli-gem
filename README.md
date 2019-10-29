796c701f263a2ba78d12d6e5a39ea2fb7818eca6
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-29 10:05:46 -0400

    Latest

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Find your favorite games and watch them live
 
-Welcome to your new gem! Find your favorite games gem!
+Welcome to the "Fine your favorite games" gem!
 
 ## Installation
 
 Add this line to your application's Gemfile:
 
 ```ruby
-gem 'zerp'
+gem ''
 ```
 
 And then execute:
@@ -16,21 +16,22 @@ And then execute:
 
 Or install it yourself as:
 
-    $ gem install zerp
+    $ gem install
 
 ## Usage
 
-TODO: Write usage instructions here
+1. Clone this repository
+2. cd to 'watch-your-favorite-games-cli-gem'
+3. Type "current_games" into your terminal
+4. You should be prompted with a list of 30 games, and asked to enter a number 0-30 that corresponds to each game. Enter a number 1-30.
+5. You should receive a URL to a live stream of the game you chose.
+6. Copy and paste the link into your browser, and enjoy watching!
 
 ## Development
 
 After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
 
 To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
-
-## Contributing
-
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/zerp.
 
 ## License
 
--- lib/api.rb
+++ lib/api.rb
@@ -6,7 +6,7 @@ require 'json'
 
 
 module Api
-
+# global arrays to hold the eventual game ID (to be inserted into second API call) and game name (to be inserted into final text to user).
 $game_id = []
 $game_name = []
 
@@ -19,7 +19,7 @@ $game_name = []
 #     end
 
     def api_call
-
+#API call to twitch.com which pulls top 30 most viewed current games
       uri = URI.parse("https://api.twitch.tv/helix/games/top?first=32")
       request = Net::HTTP::Get.new(uri)
       request["Client-Id"] = "212gsg4xr17yp12of3kmw7sha2f121"
@@ -38,16 +38,17 @@ $game_name = []
         games = []
         ids = []
 
+# pushes all games and ids into separate arrays with index
         d = c.each_with_index {|hash,i| games << "#{i}. " + hash["name"]}
         e = c.each_with_index {|hash,i| ids << hash["id"]}
-
+# shows all indexed games and prompts user
         puts games
         puts "Welcome! Above is a list of today's most popular games. Type a number 0 - 30, then press enter, to get a URL where you can watch that game live."
-
+# gets user input, turns string to integer
         f = gets.chomp
         g = f.to_i
         h = ids[g]
-
+# pushes user selection into game id array and game name array
       $game_id << h
       $game_name << games[g]
 
@@ -61,7 +62,7 @@ class Top_Player
 #end
 
   def api_call
-
+# takes user selection game id and inserts into API call. API call returns most viewed game being played
     x = $game_id[0]
     y = $game_name[0]
     uri = URI.parse("https://api.twitch.tv/helix/streams?game_id=#{x}&first=1")
@@ -75,12 +76,15 @@ class Top_Player
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
       end
+
+# parse API response to return only user name of twitch streamer
     a = response.body
     b = JSON.parse(a)
     c = b["data"]
     d = c[0]
     e = d["user_name"]
 
+# provides user with a link of their game
     puts "Copy and paste this URL into your browser to watch a live game of #{y}: https://www.twitch.tv/#{e}"
 
   end
