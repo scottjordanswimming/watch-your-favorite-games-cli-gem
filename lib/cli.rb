324c95db1a1b48e7e3b1e9ea59e38202eef8cb58
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-30 13:08:38 -0400

    really no

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,43 +1,47 @@
-# frozen_string_literal: true
-
 require 'net/http'
 require 'uri'
-require 'current_games/version'
+require "current_games/version"
 require 'json'
 require 'games'
 require 'api'
 
 class Cli
+
   attr_accessor :games, :store_games
 
-  @@game_id = ['nil']
-  @@game_name = ['nil']
+@@game_id = ["nil"]
+@@game_name = ["nil"]
 
-  def initialize
-    @store_games = store_games
-  end
+def initialize
+  @store_games = store_games
 
-  # @@game_id = []
-  #
-  #     def self.game_id
-  #       @@game_id
-  #     end
-  def first_prompt
-    # shows all indexed games and prompts user
-    Games.store_games
-    puts Games.all
-    puts ' '
-    puts "Welcome! Above is a list of today's most popular games. Type a number 1 - 25, then press enter to get a URL where you can watch that game live."
-    # gets user input, turns string to integer
-    f = gets.chomp
-    g = f.to_i
-    h = Games.ids[g]
-    # pushes user selection into game id array and game name array
-    @@game_id << h
-    @@game_name << Games.all[g]
-      end
+end
+
+# @@game_id = []
+#
+#     def self.game_id
+#       @@game_id
+#     end
+def first_prompt
+
+
+# shows all indexed games and prompts user
+        Games.store_games
+        puts Games.all
+        puts " "
+        puts "Welcome! Above is a list of today's most popular games. Type a number 1 - 25, then press enter to get a URL where you can watch that game live."
+# gets user input, turns string to integer
+        f = gets.chomp
+        g = f.to_i
+        h = Games.ids[g]
+# pushes user selection into game id array and game name array
+      @@game_id << h
+      @@game_name << Games.all[g]
+
+    end
 
   def second_prompt
+<<<<<<< HEAD
 <<<<<<< HEAD
     b = Api.api_players
     puts 'hi'
@@ -52,6 +56,8 @@ class Cli
     system('open', "https://www.twitch.tv/#{e}")
     puts ' '
 =======
+=======
+>>>>>>> parent of 403ad89... don't
     a = Api.api_players
     b = JSON.parse(a)
     c = b["data"]
@@ -60,11 +66,19 @@ class Cli
     f = @@game_name[1]
 
 # provides user with a link of their game
+<<<<<<< HEAD
 
     system("open", "https://www.twitch.tv/#{e}")
     puts " "
 >>>>>>> parent of 5dc5689... broken currently, but better
+=======
+puts d
+    system("open", "https://www.twitch.tv/#{e}")
+    puts " "
+>>>>>>> parent of 403ad89... don't
     puts "We opened a live game of #{f} in your browser at this URL: https://www.twitch.tv/#{e} - This streamer's name is #{e}."
-    puts ' '
+    puts " "
+
   end
+
 end
