403ad89c50ec050782f43e57d2915f43ba50e262
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-30 13:01:13 -0400

    don't

--- lib/api.rb
+++ lib/api.rb
@@ -1,45 +1,44 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
-require "current_games/version"
+require 'current_games/version'
 require 'json'
 require 'games'
 require 'cli'
 
 class Api
-
-    def api_top_games
-#API call to twitch.com which pulls top 30 most viewed current games
-      uri = URI.parse("https://api.twitch.tv/helix/games/top?first=26")
-      request = Net::HTTP::Get.new(uri)
-      request["Client-Id"] = "212gsg4xr17yp12of3kmw7sha2f121"
-
-      req_options = {
-        use_ssl: uri.scheme == "https",
-      }
-
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
-        end
-        response.body
-      end
-
-  def self.api_players
-# takes user selection game id and inserts into API call. API call returns most viewed game being played
-    x = Games.ids
-    y = x[0]
-    uri = URI.parse("https://api.twitch.tv/helix/streams?game_id=#{y}&first=1")
+  def api_top_games
+    # API call to twitch.com which pulls top 30 most viewed current games
+    uri = URI.parse('https://api.twitch.tv/helix/games/top?first=26')
     request = Net::HTTP::Get.new(uri)
-    request["Client-Id"] = "212gsg4xr17yp12of3kmw7sha2f121"
+    request['Client-Id'] = '212gsg4xr17yp12of3kmw7sha2f121'
 
     req_options = {
-      use_ssl: uri.scheme == "https",
+      use_ssl: uri.scheme == 'https'
     }
 
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
-      end
-      response.body
+    end
+    response.body
     end
 
+  def self.api_players
+    # takes user selection game id and inserts into API call. API call returns most viewed game being played
+    x = Games.ids
+    y = x[0]
+    uri = URI.parse("https://api.twitch.tv/helix/streams?game_id=#{y}&first=1")
+    request = Net::HTTP::Get.new(uri)
+    request['Client-Id'] = '212gsg4xr17yp12of3kmw7sha2f121'
 
+    req_options = {
+      use_ssl: uri.scheme == 'https'
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    response.body
+    end
   end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,60 +1,56 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
-require "current_games/version"
+require 'current_games/version'
 require 'json'
 require 'games'
 require 'api'
 
 class Cli
-
   attr_accessor :games, :store_games
 
-@@game_id = ["nil"]
-@@game_name = ["nil"]
+  @@game_id = ['nil']
+  @@game_name = ['nil']
 
-def initialize
-  @store_games = store_games
-
-end
-
-# @@game_id = []
-#
-#     def self.game_id
-#       @@game_id
-#     end
-def first_prompt
-
-
-# shows all indexed games and prompts user
-        Games.store_games
-        puts Games.all
-        puts " "
-        puts "Welcome! Above is a list of today's most popular games. Type a number 1 - 25, then press enter to get a URL where you can watch that game live."
-# gets user input, turns string to integer
-        f = gets.chomp
-        g = f.to_i
-        h = Games.ids[g]
-# pushes user selection into game id array and game name array
-      @@game_id << h
-      @@game_name << Games.all[g]
-
-    end
-
-  def second_prompt
-    a = Api.api_players
-    b = JSON.parse(a)
-    c = b["data"]
-    d = c[0]
-    e = d["user_name"]
-    f = @@game_name[1]
-
-# provides user with a link of their game
-puts d
-    system("open", "https://www.twitch.tv/#{e}")
-    puts " "
-    puts "We opened a live game of #{f} in your browser at this URL: https://www.twitch.tv/#{e} - This streamer's name is #{e}."
-    puts " "
-
+  def initialize
+    @store_games = store_games
   end
 
+  # @@game_id = []
+  #
+  #     def self.game_id
+  #       @@game_id
+  #     end
+  def first_prompt
+    # shows all indexed games and prompts user
+    Games.store_games
+    puts Games.all
+    puts ' '
+    puts "Welcome! Above is a list of today's most popular games. Type a number 1 - 25, then press enter to get a URL where you can watch that game live."
+    # gets user input, turns string to integer
+    f = gets.chomp
+    g = f.to_i
+    h = Games.ids[g]
+    # pushes user selection into game id array and game name array
+    @@game_id << h
+    @@game_name << Games.all[g]
+      end
+
+  def second_prompt
+    b = Api.api_players
+    puts 'hi'
+    c = JSON.parse(b)
+    d = c['data']
+    e = d[0]
+    f = e['user_name']
+    g = @@game_name[1]
+
+    # provides user with a link of their game
+    puts d
+    system('open', "https://www.twitch.tv/#{e}")
+    puts ' '
+    puts "We opened a live game of #{f} in your browser at this URL: https://www.twitch.tv/#{e} - This streamer's name is #{e}."
+    puts ' '
+  end
 end
